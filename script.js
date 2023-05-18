c551079a6517e7397b9dd50b6055f48b7de70c46
Gabriel Parada <gabrielparada05@gmail.com>
2023-05-17 23:50:07 -0400

    no module

--- script.js
+++ script.js
@@ -10,3 +10,12 @@
       documentDialect: "canadian"
     });
   });
+
+  /*import * as Grammarly from "./package-lock.json";
+
+await Grammarly.init("client_MwkwrPLsbt9oTavevWb2dF");
+
+Grammarly.addPlugin(
+  document.querySelector("textarea"),
+  { documentDialect: "british" },
+);*/
