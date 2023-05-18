4686ab8702d2788a0d738ac8156613cbc0497b8a
Gabriel Parada <gabrielparada05@gmail.com>
2023-05-18 00:02:49 -0400

    await

--- script.js
+++ script.js
@@ -1,23 +1,7 @@
- // Create a classic editor instance
- import * as Grammarly from "./package-lock.json";
- 
- const editor =  ClassicEditor.create(     //await delete before classic
-    document.getElementById("textarea")
-  );
-
-  // Configure the Grammarly Text Editor SDK
-  Grammarly.init().then((grammarly) => {
-    grammarly.addPlugin(editor.ui.getEditableElement(), {
-      autocomplete: "on",
-      documentDialect: "canadian"
-    });
-  });
-
-  /*
 
 await Grammarly.init("client_MwkwrPLsbt9oTavevWb2dF");
 
 Grammarly.addPlugin(
   document.querySelector("textarea"),
   { documentDialect: "british" },
-);*/
+);
