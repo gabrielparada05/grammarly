0b1c48cecb720f69c0fbebd9eb8c70cbbe26ee1a
Gabriel Parada <gabrielparada05@gmail.com>
2023-05-17 23:16:54 -0400

    remove await in 2nd line js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 import * as Grammarly from "./package-lock.json";
 
-await Grammarly.init("client_MwkwrPLsbt9oTavevWb2dF");
+ Grammarly.init("client_MwkwrPLsbt9oTavevWb2dF");
 
 grammarly.addPlugin(
     document.querySelector("textarea"),
